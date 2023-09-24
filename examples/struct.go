977b50cbc2ffb37649d041fb555e828f4b511eee
Maciej Kudas <maciejkudas@gmail.com>
2023-09-24 13:20:57 +0200

    docs(examples): add command examples

--- examples/struct.go
+++ examples/struct.go
@@ -7,6 +7,10 @@ import (
 	myAlias "github.com/donatorsky/go-cmder/internal/utils"
 )
 
+//go:generate go-cmder -out create_struct_cmd.go -exclude Ignore -sorted -constructor default -constructor WithIntAndString:Int,String Struct CreateStructCmd
+//go:generate go-cmder -out struct_with_includes_cmd.go -exclude Ignore -include Ignore -include Int -include String -include UniqueMultiFlag -sorted -constructor default -constructor WithIntAndStringAndUniqueMultiFlag:Int,String,UniqueMultiFlag Struct StructWithIncludesCmd
+//go:generate go-cmder -out mutable_update_struct_cmd.go -sorted -constructor default -constructor WithIntAndUniqueMultiFlag:Int,UniqueMultiFlag -mutable Struct UpdateStructCmd
+//go:generate go-cmder -sorted -constructor default Struct StructWithDefaultOutputFilenameCmd
 type Struct struct {
 	String  string
 	string  string
